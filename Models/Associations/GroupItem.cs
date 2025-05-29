3caaa6436fb0df0ad7818360cbcafd555bd13070
Heribert Gasparoli <hgasparoli@hotmail.com>
2025-05-29 11:44:21 +0200

    remove userId from GroupItems

--- Models/Associations/GroupItem.cs
+++ Models/Associations/GroupItem.cs
@@ -8,8 +8,6 @@ public class GroupItem : BaseEntity
 
     public Guid? ClientId { get; set; }
 
-    public Guid? UserId { get; set; }
-
     public Guid GroupId { get; set; }
 
     public virtual Client? Client { get; set; } = null!;
