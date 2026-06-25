a7b5e96cd46846ddc4b8d8a2e05cb478374879de
HeribertG <hgasparoli@hotmail.com>
2026-06-25 09:02:46 +0200

    chore(release): bump VersionConstant to 1.0.18

--- Application/Constants/VersionConstant.cs
+++ Application/Constants/VersionConstant.cs
@@ -6,7 +6,7 @@ public class VersionConstant
 {
     public const int CMajor = 1;
     public const int CMinor = 0;
-    public const int CPatch = 17;
+    public const int CPatch = 18;
     public const string CBuildKey = "local";
-    public const string CBuildTimestamp = "2026-06-24";
+    public const string CBuildTimestamp = "2026-06-25";
 }
